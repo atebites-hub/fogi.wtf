a141929a0ef5612c80fbb3fd4d993995ca6158c7
frozencode <jaskarnb@gmail.com>
2024-08-23 22:21:01 -0500

    updated template references to fogi

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={App} />
+        </Routes>
+      </Router>
+
       <Navbar />
       <Header />
       <Main />
       <Footer />
+
+
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
               to="headerbg"
               style={{ cursor: "pointer" }}
             >
-              DarkLeas
+              FOGI
             </Link>
           </h1>
           <ul className="bar">
